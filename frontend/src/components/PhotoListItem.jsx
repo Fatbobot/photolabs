1061563d89cc19638bcedbde6b103cecff1d0bc4
Fatbobot <Fatbobot@gmail.com>
2024-03-07 19:43:25 -0400

    add photo list component

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import PhotoFavButton from "./PhotoFavButton";
 import "../styles/PhotoListItem.scss";
 
 
@@ -7,13 +7,14 @@ const PhotoListItem = (props) => {
   const { photo } = props;
   return (
     <div className="photo-list__item">
-      <img src={photo.imageSource} alt="A photo" />
+      <img src={photo.urls.regular} alt="A photo" />
       <div className="photo-list__item-details">
-        <img src={photo.profile} alt="Profile" />
+        <img src={photo.user.profile} alt="Profile" />
         <div className="photo-list__item-details-text">
-          <h2>{photo.username}</h2>
+          <h2>{photo.user.username}</h2>
           <p>{photo.location.city}, {photo.location.country}</p>
         </div>
+        <PhotoFavButton />
       </div>
     </div>
   );
