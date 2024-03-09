602c5499b62a6bfb2f8c1a0afb14404ed8312cba
Fatbobot <Fatbobot@gmail.com>
2024-03-09 04:28:44 -0400

    fix favicon component

--- frontend/src/components/FavBadge.jsx
+++ frontend/src/components/FavBadge.jsx
@@ -6,7 +6,7 @@ import '../styles/FavBadge.scss';
 const FavBadge = ({ isFavPhotoExist }) => {
   return (
     <div className='fav-badge'>
-      <FavBadge isFavPhotoExist={props.isFavPhotoExist}/>
+      <FavIcon isFavPhotoExist={isFavPhotoExist}/>
     </div>
   ) 
 };
--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -4,12 +4,12 @@ import '../styles/TopNavigationBar.scss'
 import TopicList from './TopicList';
 
 const TopNavigation = (props) => {
-  const {topics} = props
+  const {topics, isFavPhotoExist} = props
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
       <TopicList topics = {props.topics}/>
-      <FavBadge />
+      <FavBadge isFavPhotoExist={props.isFavPhotoExist} />
     </div>
   )
 }
