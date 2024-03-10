87f8440c18bd6f1d1a15b090adce29298c47e83d
Fatbobot <Fatbobot@gmail.com>
2024-03-10 03:49:59 -0400

    add datahook to app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,30 @@
-import React, { useState } from 'react';
+import React from 'react';
 import HomeRoute from './routes/HomeRoute';
 import './App.scss';
 import MockPhotos from 'mocks/photos.js';
 import MockTopics from 'mocks/topics.js';
+import useApplicationData from './hooks/useApplicationData'; // Import the custom hook
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
+  const {
+    state,
+    updateToFavPhotoIds,
+    setPhotoSelected,
+    onClosePhotoDetailsModal,
+  } = useApplicationData(); // Use the custom hook
+
   return (
     <div className="App">
-      <HomeRoute mockTopics={MockTopics} mockPhotos={MockPhotos}/>
+      <HomeRoute 
+        mockTopics={MockTopics} 
+        mockPhotos={MockPhotos}
+        // Pass the state and functions as props
+        favoritePhotos={state.favoritePhotos}
+        updateToFavPhotoIds={updateToFavPhotoIds}
+        setPhotoSelected={setPhotoSelected}
+        onClosePhotoDetailsModal={onClosePhotoDetailsModal}
+      />
     </div>
   );
 };
