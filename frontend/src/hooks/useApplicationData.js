20cbe13c46e25839d7bc5e4c4eb0f702a33a3f19
Fatbobot <Fatbobot@gmail.com>
2024-03-10 04:23:08 -0400

    add usereducer to applicationdata

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,32 +1,53 @@
-import { useState } from 'react';
+import { useReducer } from 'react';
+
+export const ACTIONS = {
+  FAV_PHOTO_ADDED: 'FAV_PHOTO_ADDED',
+  FAV_PHOTO_REMOVED: 'FAV_PHOTO_REMOVED',
+  SET_PHOTO_DATA: 'SET_PHOTO_DATA',
+  SET_TOPIC_DATA: 'SET_TOPIC_DATA',
+  SELECT_PHOTO: 'SELECT_PHOTO',
+  DISPLAY_PHOTO_DETAILS: 'DISPLAY_PHOTO_DETAILS'
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case ACTIONS.FAV_PHOTO_ADDED:
+      return { ...state, favoritePhotos: [...state.favoritePhotos, action.payload] };
+    case ACTIONS.FAV_PHOTO_REMOVED:
+      return { ...state, favoritePhotos: state.favoritePhotos.filter(id => id !== action.payload) };
+    case ACTIONS.SET_PHOTO_DATA:
+      return { ...state, photoData: action.payload };
+    case ACTIONS.SET_TOPIC_DATA:
+      return { ...state, topicData: action.payload };
+    case ACTIONS.SELECT_PHOTO:
+      return { ...state, selectedPhoto: action.payload };
+    case ACTIONS.DISPLAY_PHOTO_DETAILS:
+      return { ...state, isModalOpen: action.payload };
+    default:
+      throw new Error(
+        `Tried to reduce with unsupported action type: ${action.type}`
+      );
+  }
+}
 
 const useApplicationData = () => {
-  const [state, setState] = useState({ favoritePhotos: [] });
-  const [singlePhotoDetail, setSinglePhotoDetail] = useState(null);
-  const [displayModal, setDisplayModal] = useState(false);
+  const [state, dispatch] = useReducer(reducer, { favoritePhotos: [] });
 
   const updateToFavPhotoIds = (photoId) => {
-    setState(prevState => {
-      if (prevState.favoritePhotos.includes(photoId)) {
-        return {
-          ...prevState,
-          favoritePhotos: prevState.favoritePhotos.filter(id => id !== photoId),
-        };
-      } else {
-        return {
-          ...prevState,
-          favoritePhotos: [...prevState.favoritePhotos, photoId],
-        };
-      }
-    });
+    if (state.favoritePhotos.includes(photoId)) {
+      dispatch({ type: ACTIONS.FAV_PHOTO_REMOVED, payload: photoId });
+    } else {
+      dispatch({ type: ACTIONS.FAV_PHOTO_ADDED, payload: photoId });
+    }
   };
+
   const handlePhotoClick = (photo) => {
-    setSinglePhotoDetail(photo);
-    setDisplayModal(true)
+    dispatch({ type: ACTIONS.SELECT_PHOTO, payload: photo });
+    dispatch({ type: ACTIONS.DISPLAY_PHOTO_DETAILS, payload: true });
   };
 
   const closeModal = () => {
-    setDisplayModal(false);
+    dispatch({ type: ACTIONS.DISPLAY_PHOTO_DETAILS, payload: false });
   };
 
   return {
